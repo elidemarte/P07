64305dcfa21cfaa7bb1cd3a694797e5101fb2926
vilynn <59678048+vi1ynn@users.noreply.github.com>
2024-04-11 00:22:11 -0500

    SortedTaskList done
    mostly ^ only one thing idk how to do

--- src/SortedTaskList.java
+++ src/SortedTaskList.java
@@ -12,9 +12,16 @@ public class SortedTaskList extends TaskList {
             throw new IllegalArgumentException("Task is already in list. Duplicate items are not " +
                     "allowed in this list.");
         }
-        //if
 
+        // TODO this only works if sortingOrder is for PRIORITY... idk how to call setSortingOrderByPriority
+        // finds index where the new task should be inserted
+        int index = 0;
+        while (this.get(index).compareTo(aTask) < 0) {
+            index++;
+        }
 
+        // inserts new task at the correct index
+        super.add(index, aTask);
     }
 
     public void addFirst(Task aTask) throws NullPointerException, IllegalStateException {
@@ -23,13 +30,23 @@ public class SortedTaskList extends TaskList {
             throw new NullPointerException("Task to add can't be null");
         }
         // check to see if task can be added to the head of this list
-        if (!isEmpty() && aTask.compareTo(/*first element in task list */) > 0) {
+        if (!isEmpty() && aTask.compareTo(this.get(0)) > 0) {
             throw new IllegalStateException("Task can not be added to the head/start of this list");
         }
 
-        if (aTask.compareTo())
+        // iterates through tasklist checking if aTask is less than all other elements
+        boolean smallest = true;
+        for (int i = 1; i < this.size(); i++) {
+            if (aTask.compareTo(this.get(i)) > 0) {
+                smallest = false;
+                break;
+            }
+        }
 
-
+        // if less than all other elements, adds to first
+        if (smallest) {
+            super.add(0, aTask);
+        }
     }
 
     public void add(int index, Task aTask) throws NullPointerException, IllegalStateException,
@@ -39,30 +56,46 @@ public class SortedTaskList extends TaskList {
         if (aTask == null) {
             throw new NullPointerException("Task to add can't be null");
         }
-        //check if index is valid
+        // check if index is valid
         if (index < 0 || index > size()){
             throw new IndexOutOfBoundsException("Index was out of bounds of the list");
         }
 
-        // check to see if task can be added with respect to surrounding indicies priority level
-        //TODO: complete this part of method (checks to throw Illegal state exception)
-
-        // TODO: need to get reference of index
-        for (int i = 0; i < size(); i++) {
-
-        }
-        if (aTask.compareTo() {
-            throw new IllegalStateException("Task can't be added to specified index.");
+        // checks to see if task can be added with respect to surrounding indicies priority level
+        // if before task exists, check if aTask is greater than it
+        if (index - 1 >= 0 && this.get(index - 1).compareTo(aTask) > 0) {
+            throw new IllegalStateException("Task cannot be added at the incorrectly sorted index.");
         }
 
+        // if following task exists, check if aTask is smaller than it
+        if (index + 1 <= this.size() - 1 && this.get(index + 1).compareTo(aTask) < 0) {
+            throw new IllegalStateException("Task cannot be added at the incorrectly sorted index.");
+        }
 
-
+        // otherwise add it to the tasklist
+        super.add(index, aTask);
     }
 
     public Task peekBest() throws NoSuchElementException {
+        // check if list is empty
         if (isEmpty()) {
             throw new NoSuchElementException("List is already empty");
         }
-        //return best task
+
+        // otherwise return index 0 task
+        return this.get(0);
+    }
+
+    public Task removeBest() throws NoSuchElementException {
+        // check if list is empty
+        if (isEmpty()) {
+            throw new NoSuchElementException("List is already empty");
+        }
+
+        // makes temporary best task then removes
+        Task best = this.get(0);
+        this.remove(0);
+
+        return best;
     }
 }
